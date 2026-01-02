75a78c4fbf60c3e5b370ea4d27f464ce5ffb4565
JerssonFU <jersson.zynths@gmail.com>
2026-01-02 15:55:10 -0500

    V2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/deploy-example/", // Asegurar que GitHub Pages use la ruta correcta
+  base: "/Portafolio-BI/", // Asegurar que GitHub Pages use la ruta correcta
 });
